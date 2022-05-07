3ded49e29069947db28b3bcc218be758bae8f24b
Brian Ritchie <sayhello@brianritchie.me>
2022-05-07 22:18:11 +1000

    added list of homebrew packages

--- mac/homebrew-packages.sh
+++ mac/homebrew-packages.sh
@@ -7,3 +7,34 @@
 
 which -s brew
 
+# Install Homebrew Packages
+
+brew install --cask iterm2
+brew install git
+brew install romkatv/powerlevel10k/powerlevel10k\necho "source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
+brew install --cask dropbox
+brew install --cask alfred
+brew install --cask google-chrome
+brew install --cask keepassxc
+brew install --cask spotify
+brew install --cask slack
+brew install --cask discord
+brew install --cask insomnia
+brew install --cask gitkraken
+brew install --cask visual-studio-code
+brew install --cask vim
+brew install vim
+brew install neovim
+brew install --cask chromium
+brew install --cask kitty
+brew install --cask whatsapp
+brew install git
+brew install --cask evernote
+brew install rust
+brew install --cask tidal
+brew install --cask iina
+brew install --cask caffeine
+brew install wget
+brew install exa
+brew install librewolf --no-quarantine
+brew install zsh-autosuggestions
--- mac/install.sh
+++ mac/install.sh
@@ -2,3 +2,8 @@
 
 ## Install Mac Binaries and Applications with Homebrew
 
+
+## Install ZSH Plugins
+
+# AutoSuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
